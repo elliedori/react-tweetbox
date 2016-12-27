5cbec328214791159a03dca5e59a823d06df4a66
elliedori <elliedori@gmail.com>
2016-12-27 15:45:48 -0800

    Add responsive add photo button and character count

--- src/App.js
+++ src/App.js
@@ -23,20 +23,35 @@ export default App;
 
 var TweetBox = React.createClass({
   getInitialState: function() {
-  return {
-      text: ""
+    return {
+      text: "",
+      photoAdded: false
     };
   },
   handleChange: function(event) {
-  this.setState({text: event.target.value});
+    this.setState({ text: event.target.value });
+  },
+  remainingCharacters: function() {
+    if (this.state.photoAdded) {
+    return 140 - 23 - this.state.text.length;
+    } else {
+    return 140 - this.state.text.length;
+    }
+  },
+  togglePhoto: function(event) {
+    this.setState({photoAdded: !this.state.photoAdded });
   },
   render: function() {
     return (
       <div className="well clearfix">
-        <textarea className="form-control" onChange={this.handleChange}></textarea>
+        <textarea className="form-control"
+                  onChange={this.handleChange}>
+        </textarea>
         <br/>
-        <span>{140 - this.state.text.length}</span>
-        <button className="btn btn-primary pull-right" disabled={this.state.text.length === 0}>Tweet</button>
+        <span>{ this.remainingCharacters() }</span>
+        <button className="btn btn-primary pull-right"
+                disabled={this.remainingCharacters() === 140 }>Tweet</button>
+        <button className="btn btn-default pull-right" onClick={this.togglePhoto}>{this.state.photoAdded ? "✓ Photo Added" : "Add Photo" }</button>
       </div>
     );
   }
